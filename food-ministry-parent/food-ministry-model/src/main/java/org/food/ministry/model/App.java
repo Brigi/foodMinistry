d06b3b27458f3b95e9575a4bbe1fc2b3ebfba083
Brigi <maximilian.briglmeier@gmail.com>
2019-02-09 12:49:03 +0100

    Created project structure via Maven reactor build

---
+++ food-ministry-parent/food-ministry-model/src/main/java/org/food/ministry/model/App.java
@@ -0,0 +1,13 @@
+package org.food.ministry.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
